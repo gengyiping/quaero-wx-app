a1e3cdd2b4fc4c82a70a8a8eec317e753a571742
jason-luo-jie <626781417@qq.com>
2020-03-18 09:37:57 +0800

    zhenghedaima-luojie

---
+++ pages/after-salesProgress/tabbar/tabbar.js
@@ -0,0 +1,50 @@
+// pages/after-salesProgress/tabbar/tabbar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tabBar: [
+
+      { 
+        "pagePath": "/pages/index/index",
+        "iconPath": "../../img/home.png",
+        "selectedIconPath": "../../img/home.png",
+        "text": "主页"
+      },
+      {
+        "pagePath": "pages/scan/scan",
+        "iconPath": "../../img/scan.png",
+        "selectedIconPath": "../../img/scan.png",
+        "text": "转交"
+
+      }, 
+      {
+        "current": 0,
+        "pagePath": "pages/user/user",
+        "iconPath": "../../img/mine.png",
+        "selectedIconPath": "../../img/mine.png",
+        "text": "个人中心"
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    navigateDetail: function (e) {
+      wx.reLaunch({ // 关闭所有打开过的页面，跳转到相对于的页面
+        url: e.currentTarget.dataset.url  // 获取tabbar.wxml中data-url传递的参数
+      })
+    }
+  }
+})
+
+
