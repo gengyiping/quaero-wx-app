e58c085b7fcd3d0ba82e240f486734a1fd2386fd
gengyp <gengyiping@163.com>
2020-06-17 13:50:07 +0800

    solve the problem of the  wx.requestSubscribeMessage api can't display in onLoad function

--- pages/index/index.js
+++ pages/index/index.js
@@ -36,22 +36,39 @@ Page({
     wx.requestSubscribeMessage({
       tmplIds: ['sbB7c9RezqyN7kAdavARBYF7BzpXHaGXUgm5bmjZnr8'],
       success(res) {
-        console.log('授权成功')
+        console.log('授权成功2')
       },
       fail: function (res) {
-        console.log('授权失败')
+        console.log('授权失败2')
       }
     })
   },
   onLoad: function (options) {
-    wx.requestSubscribeMessage({
-      tmplIds: ['sbB7c9RezqyN7kAdavARBYF7BzpXHaGXUgm5bmjZnr8'],
-      success(res) {
-        console.log('授权成功' )
-      },
-      fail: function (res) {
-        console.log('授权失败')
+    console.log("onLoad")
+
+    wx.showModal({
+      title: '接收通知消息',
+      content: '请点击确定->勾选维修任务通知->勾选总是保持以上选择，不再询问->点击允许',
+      success (res) {
+
+        wx.requestSubscribeMessage({
+          tmplIds: ['sbB7c9RezqyN7kAdavARBYF7BzpXHaGXUgm5bmjZnr8'],
+          success(res) {
+            console.log('授权成功' )
+          },
+          fail: function (res) {
+            console.log('授权失败',res)
+          }
+        })
+        if (res.confirm) {
+          console.log('用户点击确定')
+          } else if (res.cancel) {
+          console.log('用户点击取消')
+          }
+      
       }
-    })
+    
+  })
+
   },
 })
